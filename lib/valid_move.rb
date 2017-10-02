2af356cda4c6f67e35a44b17975ad992aedc0699
Melissa Sanchez <melissa90sanchez@gmail.com>
2017-10-02 11:22:45 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board,index)
-  if position_taken?
-    return true
-  else
+  if position_taken?[0,8] == !position_taken
     return false
   end
 end
